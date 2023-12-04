1e3b75957d999a341d016e6f437daaf7bb109328
honeykokoro <145886073+honeykokoro@users.noreply.github.com>
2023-12-04 16:30:00 +0800

    rm1campaign.js

---
+++ rm1campaign.js
@@ -0,0 +1,21 @@
+document.getElementById('myForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    alert("Submission Successful!");
+    this.reset();
+});
+window.onscroll = function() {
+    scrollFunction();
+};
+function scrollFunction() {
+    var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+}
+function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
